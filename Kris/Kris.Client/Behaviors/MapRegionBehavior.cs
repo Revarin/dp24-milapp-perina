ecaba7257d59ab66a6b8eb0f25d0da58f5a18dc9
Revarin <dan.perina2@gmail.com>
2023-09-05 15:37:21 +0200

    MAUI migration

---
+++ Kris/Kris.Client/Behaviors/MapRegionBehavior.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Maui.Controls.Maps;
+using Microsoft.Maui.Maps;
+
+using MauiMap = Microsoft.Maui.Controls.Maps.Map;
+
+namespace Kris.Client.Behaviors
+{
+    public class MapRegionBehavior : BindableBehavior<MauiMap>
+    {
+        public static readonly BindableProperty CurrentRegionProperty = BindableProperty.Create(
+            "CurrentRegion", typeof(MapSpan), typeof(MapRegionBehavior), default(MapSpan), BindingMode.TwoWay,
+            propertyChanged: OnCurrentRegionChanged);
+
+        public MapSpan CurrentRegion
+        {
+            get { return (MapSpan)GetValue(CurrentRegionProperty); }
+            set { SetValue(CurrentRegionProperty, value); }
+        }
+
+        private static void OnCurrentRegionChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var thisInstance = (bindable as MapRegionBehavior)?.AssociatedObject;
+            var newRegion = newValue as MapSpan;
+
+            thisInstance?.MoveToRegion(newRegion);
+        }
+    }
+}
--- Kris/Kris.Client/ViewModels/MapViewModel.cs
+++ Kris/Kris.Client/ViewModels/MapViewModel.cs
@@ -1,6 +1,30 @@
-﻿namespace Kris.Client.ViewModels
+﻿using Microsoft.Maui.Maps;
+using System.Windows.Input;
+
+namespace Kris.Client.ViewModels
 {
     public class MapViewModel : ViewModelBase
     {
+        private MapSpan _currentRegion;
+        public MapSpan CurrentRegion
+        {
+            get { return _currentRegion; }
+            set { SetPropertyValue(ref _currentRegion, value); }
+        }
+
+        public ICommand TestCommand { get; private set; }
+
+        public MapViewModel()
+        {
+            CurrentRegion = MapSpan.FromCenterAndRadius(new Location(), Distance.FromKilometers(4000));
+            TestCommand = new Command(Test);
+        }
+
+        private void Test()
+        {
+            var nspan = CurrentRegion;
+            var ncenter = new Location(nspan.Center.Latitude + 5, nspan.Center.Longitude);
+            CurrentRegion = MapSpan.FromCenterAndRadius(ncenter, nspan.Radius);
+        }
     }
 }
