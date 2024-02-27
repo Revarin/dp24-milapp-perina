6abfe9560e59da2679fe3f2b93f2eac3b6ae1f89
Revarin <dan.perina2@gmail.com>
2024-02-27 14:32:13 +0100

    Fix missing edit user interface

--- Kris/Common/Kris.Interface/Requests/EditUserRequest.cs
+++ Kris/Common/Kris.Interface/Requests/EditUserRequest.cs
@@ -2,6 +2,7 @@
 
 public sealed class EditUserRequest : RequestBase
 {
-    public required string Login { get; set; }
+    public required string NewLogin { get; set; }
+    public required string NewPassword { get; set; }
     public required string Password { get; set; }
 }
